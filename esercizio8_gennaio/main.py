e1fe9ced892a86b2d16bbebc47fa3c05d35066d3
Serafina Marika Di Bari <marikaserafina@gmail.com>
2026-01-08 16:23:21 +0100

    add news

---
+++ esercizio8_gennaio/main.py
@@ -0,0 +1,12 @@
+"""
+Per il pomeriggio avete questo obiettivo:
+Individuate un problema concreto che si possa risolvere con un piccolo programma in Python eseguibile da linea di comando.
+Scrivete le specifiche: cosa deve fare, quali input deve accettare, quali output deve produrre
+Poi provate a realizzarlo usando uno degli strumenti gratuiti che abbiamo visto questa mattina, sfruttando soprattutto le conoscenze che avete già.
+Se volete, tenetelo piccolo: l’importante è che sia chiaro e funzionante.
+
+__VORREI ESEGUIRE UN PROGRAMMA__:
+1. DEVE CONTEGGIARE I BISCOTTI PRESENTI IN UNO SCAFFALE DEL SUPERMERCATO 
+2. COME INPUT DEVE RICEVERE IL NUMERO DI CONFEZIONI SCARICATE NEL MAGAZZINO, IL NUMERO DI PEZZI PRESENTI NELLO SCAFFALE E PEZZI VENDUTI
+3. DEVE PRODURRE IL NUMERO DI BISCOTTI PRESENTI IN MAGAZZINO, VENDUTI E I RIMAMENTI PEZZI PRESNTI NELLO SCAFFALE 
+"""
